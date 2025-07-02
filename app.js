5ff8321c8f2a8561dede877b3e4a294413c9af84
José Fernando Gómez <jofergoz92@gmail.com>
2025-07-02 10:50:03 -0300

    storage

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
-// function miGasto(nombre, fecha, monto) {
-//   this.nombre = nombre;
-//   this.fecha = fecha;
-//   this.comto = monto;
-// }
+const formIngreso = document.querySelector(".form-ingreso");
+const formGasto = document.querySelector(".form-gasto");
 
-// const miGasto1 = new miGasto("restoran", "mayo", 300);
-// const miGasto2 = new miGasto("auto", "febrero", 4500);
+// mostrar lo datos cargados
+datosGuardados();
 
-// console.log(miGasto1, miGasto2);
+// formularios
+formIngreso.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const ingreso = {
+    tipo: "Ingreso",
+    nombre: document.getElementById("nombre-ingreso").value,
+    monto: parseFloat(document.getElementById("monto-ingreso").value),
+    fecha: document.getElementById("fecha-ingreso").value,
+  };
+
+  guardarEnLocalStorage("ingresos", ingreso);
+  formIngreso.reset();
+});
+
+formGasto.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const gasto = {
+    tipo: "Gasto",
+    nombre: document.getElementById("nombre-gasto").value,
+    monto: parseFloat(document.getElementById("monto-gasto").value),
+    fecha: document.getElementById("fecha-gasto").value,
+  };
+
+  guardarEnLocalStorage("gastos", gasto);
+  formGasto.reset();
+});
+
+// guardar datos tabla
+function guardarEnLocalStorage(clave, nuevoItem) {
+  const listaActual = JSON.parse(localStorage.getItem(clave)) || [];
+  listaActual.push(nuevoItem);
+  localStorage.setItem(clave, JSON.stringify(listaActual));
+  datosGuardados();
+}
